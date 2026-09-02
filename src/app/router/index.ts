fe70028ac9a54a895d174c30656ac98f179c56da
amirhossein <amir0hossin0pr@gmail.com>
2026-09-02 23:52:32 +0330

    Update index.ts

--- src/app/router/index.ts
+++ src/app/router/index.ts
@@ -1,39 +1,40 @@
-import { createRouter, createWebHistory, type RouteRecordRaw } from 'vue-router';
-import { useCollectionStore } from '@stores/useCollectionStore';
+import {createRouter, createWebHistory, type RouteRecordRaw} from 'vue-router';
+import {useCollectionStore} from '@stores/useCollectionStore';
 
 const routes: RouteRecordRaw[] = [
-  {
-    path: '/',
-    component: () => import('../../modules/workspace/WorkspaceLayout.vue'),
-    children: [
-      { path: '', name: 'home', component: () => import('@modules/workspace/HomeView.vue') },
-      {
-        path: 'collections/:collectionId',
-        name: 'collection',
-        component: () => import('../../modules/workspace/CollectionView.vue'),
-      },
-      {
-        path: 'collections/:collectionId/requests/:requestId',
-        name: 'collectionRequest',
-        component: () => import('../../modules/workspace/RequestView.vue'),
-      },
-    ],
-  },
-  { path: '/:pathMatch(.*)*', redirect: '/' },
+    {
+        path: '/',
+        component: () => import('@modules/workspace/WorkspaceLayout.vue'),
+        children: [
+            {path: '', name: 'home', component: () => import('@modules/workspace/HomeView.vue')},
+            {
+                path: 'collections/:collectionId',
+                name: 'collection',
+                component: () => import('@modules/workspace/NodeView.vue')
+            },
+            {
+                path: 'collections/:collectionId/folders/:folderId',
+                name: 'node',
+                component: () => import('@modules/workspace/NodeView.vue')
+            },
+            {
+                path: 'collections/:collectionId/requests/:requestId',
+                name: 'request',
+                component: () => import('@modules/workspace/RequestView.vue')
+            },
+        ],
+    },
+    {path: '/:pathMatch(.*)*', redirect: '/'},
 ];
 
 export const router = createRouter({
-  history: createWebHistory(),
-  routes,
+    history: createWebHistory(),
+    routes,
 });
 
 router.beforeEach((to) => {
-  const store = useCollectionStore();
-  if (to.params.collectionId && typeof to.params.collectionId === 'string') {
-    store.selectCollection(to.params.collectionId);
-  }
-  if (to.params.requestId && typeof to.params.requestId === 'string') {
-    store.selectRequest(to.params.requestId);
-  }
-  return true;
+    const store = useCollectionStore();
+    if (typeof to.params.collectionId === 'string') store.selectCollection(to.params.collectionId);
+    if (typeof to.params.requestId === 'string') store.selectRequest(to.params.requestId);
+    return true;
 });
