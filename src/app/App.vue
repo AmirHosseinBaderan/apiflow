d8af3ba1a7d7cf5eece2968141fbe89f9f376f03
amirhossein <amir0hossin0pr@gmail.com>
2026-09-02 23:43:07 +0330

    Update App.vue

--- src/app/App.vue
+++ src/app/App.vue
@@ -2,9 +2,11 @@
   <v-app>
     <router-view />
     <AppNotifier />
+    <AppDialog />
   </v-app>
 </template>
 
 <script setup lang="ts">
 import AppNotifier from '@components/AppNotifier.vue';
+import AppDialog from '@components/AppDialog.vue';
 </script>
