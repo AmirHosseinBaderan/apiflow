ef4fba36124970ccb1b66b5319398255787a56a7
amirhossein <amir0hossin0pr@gmail.com>
2026-09-03 02:51:36 +0330

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -19,6 +19,7 @@ export default defineConfig({
       '@composables': fileURLToPath(new URL('./src/composables', import.meta.url)),
       '@modules': fileURLToPath(new URL('./src/modules', import.meta.url)),
       '@shared': fileURLToPath(new URL('./src/shared', import.meta.url)),
+      '@i18n': fileURLToPath(new URL('./src/i18n', import.meta.url)),
     },
   },
   server: { port: 5173 },
