82dc5a509db13c32f270906b4be7ee361e963d82
amirhossein <amir0hossin0pr@gmail.com>
2026-09-03 02:41:08 +0330

    Update App.vue

--- src/app/App.vue
+++ src/app/App.vue
@@ -10,9 +10,9 @@
 import { useTheme } from 'vuetify';
 import { watch, onMounted } from 'vue';
 import { useSettingsStore } from '@stores/useSettingsStore';
-import { useLocaleStore } from '@i18n/store';
 import AppNotifier from '@components/AppNotifier.vue';
 import AppDialog from '@components/AppDialog.vue';
+import {useLocaleStore} from "../i18n/store";
 
 const theme = useTheme();
 const settings = useSettingsStore();
