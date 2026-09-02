11eb1ef525210db757ce0d8d70b9e108030dfc71
amirhossein <amir0hossin0pr@gmail.com>
2026-09-02 23:40:23 +0330

    Update TestEditor.vue

--- src/components/TestEditor.vue
+++ src/components/TestEditor.vue
@@ -1,7 +1,9 @@
 <template>
   <div>
     <v-row v-for="t in local" :key="t.id" dense class="mb-1" align="center">
-      <v-col cols="3"><v-text-field v-model="t.name" placeholder="name" density="compact" hide-details /></v-col>
+      <v-col cols="3"
+        ><v-text-field v-model="t.name" placeholder="name" density="compact" hide-details
+      /></v-col>
       <v-col cols="3">
         <v-select
           v-model="t.kind.type"
@@ -14,7 +16,7 @@
       </v-col>
       <v-col v-if="t.kind.type === 'script'" cols="12">
         <v-textarea
-          v-model="t.expression"
+          v-model="t.kind.source"
           label="JS script"
           placeholder="return response.status === 200"
           density="compact"
@@ -23,7 +25,12 @@
         />
       </v-col>
       <v-col v-else>
-        <v-text-field v-model="t.expression" placeholder="expression / path / value" density="compact" hide-details />
+        <v-text-field
+          v-model="t.expression"
+          placeholder="expression / path / value"
+          density="compact"
+          hide-details
+        />
       </v-col>
       <v-col cols="auto">
         <v-btn icon="mdi-delete" size="small" variant="text" @click="remove(t.id)" />
@@ -49,12 +56,20 @@ const local = computed({
 });
 
 function add() {
-  const step: TestStep = { id: createId('tst'), name: 'Status is 200', kind: { type: 'statusEquals', value: 200 }, expression: '200' };
+  const step: TestStep = {
+    id: createId('tst'),
+    name: 'Status is 200',
+    kind: { type: 'statusEquals', value: 200 },
+    expression: '200',
+  };
   emit('update:modelValue', [...props.modelValue, step]);
 }
 
 function remove(id: string) {
-  emit('update:modelValue', props.modelValue.filter((t) => t.id !== id));
+  emit(
+    'update:modelValue',
+    props.modelValue.filter((t) => t.id !== id),
+  );
 }
 
 function onKindChange(id: string, type: string) {
@@ -62,11 +77,20 @@ function onKindChange(id: string, type: string) {
     if (t.id !== id) return t;
     let kind: TestKind;
     switch (type) {
-      case 'statusEquals': kind = { type: 'statusEquals', value: 200 }; break;
-      case 'bodyExists': kind = { type: 'bodyExists', path: '' }; break;
-      case 'durationLessThan': kind = { type: 'durationLessThan', valueMs: 1000 }; break;
-      case 'script': kind = { type: 'script', source: 'return response.status === 200' }; break;
-      default: kind = { type: 'statusEquals', value: 200 };
+      case 'statusEquals':
+        kind = { type: 'statusEquals', value: 200 };
+        break;
+      case 'bodyExists':
+        kind = { type: 'bodyExists', path: '' };
+        break;
+      case 'durationLessThan':
+        kind = { type: 'durationLessThan', valueMs: 1000 };
+        break;
+      case 'script':
+        kind = { type: 'script', source: 'return response.status === 200' };
+        break;
+      default:
+        kind = { type: 'statusEquals', value: 200 };
     }
     return { ...t, kind };
   });
