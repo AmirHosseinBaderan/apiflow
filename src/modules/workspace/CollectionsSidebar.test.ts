9d72578c846b5f342b2def1df7a08a1ab579e8bf
amirhossein <amir0hossin0pr@gmail.com>
2026-09-02 23:40:27 +0330

    Update CollectionsSidebar.test.ts

--- src/modules/workspace/CollectionsSidebar.test.ts
+++ src/modules/workspace/CollectionsSidebar.test.ts
@@ -1,4 +1,4 @@
-import { describe, expect, it } from 'vitest';
+import { describe, expect, it, beforeAll } from 'vitest';
 import { mount } from '@vue/test-utils';
 import { defineComponent } from 'vue';
 import { createPinia, setActivePinia } from 'pinia';
