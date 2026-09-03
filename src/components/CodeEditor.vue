475eb60678ab19973630ced406da60a2acd521fc
amirhossein <amir0hossin0pr@gmail.com>
2026-09-03 15:25:35 +0330

    Update CodeEditor.vue

--- src/components/CodeEditor.vue
+++ src/components/CodeEditor.vue
@@ -1,15 +1,6 @@
 <template>
   <div class="jce">
-    <pre
-      ref="mirror"
-      class="jce__mirror"
-      aria-hidden="true"
-      :style="editorStyle"
-    ><span
-v-for="(t, i) in tokens"
-           :key="i"
-:class="`jce__tok--${t.type}`"
->{{ t.text }}</span></pre>
+    <pre class="jce__mirror" ref="mirror" :style="editorStyle" aria-hidden="true"><span v-for="(t, i) in tokens" :key="i" :class="`jce__tok--${t.type}`">{{ t.text }}</span></pre>
     <textarea
       ref="area"
       :value="text"
