02a16e9f384634af536e46fc38d9968cb69d2fb9
amirhossein <amir0hossin0pr@gmail.com>
2026-09-03 19:22:51 +0330

    Update vuetify.ts

--- src/app/providers/vuetify.ts
+++ src/app/providers/vuetify.ts
@@ -1,36 +1,36 @@
 import 'vuetify/styles';
 import '@mdi/font/css/materialdesignicons.css';
-import { createVuetify } from 'vuetify';
-import { VTreeview } from 'vuetify/labs/VTreeview';
+import {createVuetify} from 'vuetify';
+import {VTreeview} from 'vuetify/labs/VTreeview';
 
 export const vuetify = createVuetify({
-  components: { VTreeview },
-  defaults: {
-    VBtn: { variant: 'flat', density: 'comfortable', size: 'medium', rounded: 'lg' },
-    VTextField: { variant: 'outlined', density: 'comfortable', hideDetails: 'auto', rounded: 'lg' },
-    VSelect: { variant: 'outlined', density: 'comfortable', hideDetails: 'auto', rounded: 'lg' },
-    VTextarea: { variant: 'outlined', density: 'comfortable', hideDetails: 'auto', rounded: 'lg' },
-    VCard: { variant: 'elevated', rounded: 'lg' },
-  },
-  theme: {
-    defaultTheme: 'dark',
-    themes: {
-      light: {
-        dark: false,
-        colors: {
-          primary: '#1867C0',
-          secondary: '#5CBBF6',
-          surface: '#FFFFFF',
-          background: '#F4F5F8',
-        },
-      },
-      dark: {
-        dark: true,
-        colors: {
-          primary: '#5CBBF6',
-          secondary: '#1867C0',
-        },
-      },
+    components: {VTreeview},
+    defaults: {
+        VBtn: {rounded: 'lg'},
+        VTextField: {variant: 'outlined', density: 'comfortable', hideDetails: 'auto', rounded: 'lg'},
+        VSelect: {variant: 'outlined', density: 'comfortable', hideDetails: 'auto', rounded: 'lg'},
+        VTextarea: {variant: 'outlined', density: 'comfortable', hideDetails: 'auto', rounded: 'lg'},
+        VCard: {variant: 'elevated', rounded: 'lg'},
     },
-  },
+    theme: {
+        defaultTheme: 'dark',
+        themes: {
+            light: {
+                dark: false,
+                colors: {
+                    primary: '#1867C0',
+                    secondary: '#5CBBF6',
+                    surface: '#FFFFFF',
+                    background: '#F4F5F8',
+                },
+            },
+            dark: {
+                dark: true,
+                colors: {
+                    primary: '#5CBBF6',
+                    secondary: '#1867C0',
+                },
+            },
+        },
+    },
 });
