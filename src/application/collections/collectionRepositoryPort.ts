2b875bd94cb3c8bd80f9cd9887982561aebb4db3
amirhossein <amir0hossin0pr@gmail.com>
2026-09-04 01:23:29 +0330

    Update collectionRepositoryPort.ts

--- src/application/collections/collectionRepositoryPort.ts
+++ src/application/collections/collectionRepositoryPort.ts
@@ -1,10 +1,16 @@
 import type { Collection } from '@domain/collection/Collection';
+import type { RequestDefinition } from '@domain/request/RequestDefinition';
 
 export interface CollectionRepository {
   list(): Promise<ReadonlyArray<Collection>>;
   get(id: string): Promise<Collection | undefined>;
   save(collection: Collection): Promise<void>;
   remove(id: string): Promise<void>;
+  getRequest(collectionId: string, requestId: string): Promise<RequestDefinition | undefined>;
+  saveRequest(collectionId: string, request: RequestDefinition): Promise<void>;
+  deleteRequest(collectionId: string, requestId: string): Promise<void>;
+  updateVariables(collectionId: string, variables: Collection['variables']): Promise<void>;
+  updateWorkflows(collectionId: string, workflows: Collection['workflows']): Promise<void>;
 }
 
 let active: CollectionRepository | undefined;
